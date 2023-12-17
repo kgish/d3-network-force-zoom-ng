bdd940d47a7d3f679ad1d52990ac0fabd3579200
Kiffin Gish <kiffin.gish@planet.nl>
2023-12-17 21:09:00 +0100

    wip

--- README.md
+++ README.md
@@ -6,6 +6,93 @@ D3.js network force demo using Angular with zoom and pan.
 
 "Write code in javascript using the d3.js library to zoom and pan a force directed network graph."
 
+```
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zoomable Force-Directed Network</title>
+    <script src="https://d3js.org/d3.v7.min.js"></script>
+    <style>
+        /* Define CSS styles for the SVG container */
+        #network-container {
+            width: 100%;
+            height: 100vh;
+            overflow: hidden;
+        }
+    </style>
+</head>
+<body>
+    <div id="network-container"></div>
+    <script>
+        // Define your graph data (you can replace this with your own data)
+        const nodes = [
+            { id: "A" },
+            { id: "B" },
+            { id: "C" },
+            // Add more nodes as needed
+        ];
+
+        const links = [
+            { source: "A", target: "B" },
+            { source: "B", target: "C" },
+            // Add more links as needed
+        ];
+
+        // Create an SVG container for the graph
+        const svg = d3.select("#network-container")
+            .append("svg")
+            .attr("width", "100%")
+            .attr("height", "100%");
+
+        // Create a simulation for the force-directed layout
+        const simulation = d3.forceSimulation(nodes)
+            .force("link", d3.forceLink(links).distance(100))
+            .force("charge", d3.forceManyBody().strength(-200))
+            .force("center", d3.forceCenter(svg.attr("width") / 2, svg.attr("height") / 2));
+
+        // Create the links and nodes
+        const link = svg.selectAll(".link")
+            .data(links)
+            .enter()
+            .append("line")
+            .attr("class", "link");
+
+        const node = svg.selectAll(".node")
+            .data(nodes)
+            .enter()
+            .append("circle")
+            .attr("class", "node")
+            .attr("r", 10);
+
+        // Define the zoom behavior
+        const zoom = d3.zoom()
+            .scaleExtent([0.1, 4])
+            .on("zoom", () => {
+                svg.attr("transform", d3.event.transform);
+            });
+
+        // Apply the zoom behavior to the SVG container
+        svg.call(zoom);
+
+        // Update the simulation on each tick
+        simulation.on("tick", () => {
+            link
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
+
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+        });
+    </script>
+</body>
+</html>
+```
+
 ## References
 
 * [D3,js](https://d3js.org)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,124 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// D3.js
+import {
+  forceCenter,
+  forceCollide,
+  forceLink,
+  forceManyBody,
+  forceSimulation, forceX, forceY,
+  select,
+  zoom
+} from 'd3';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ CommonModule, RouterOutlet, MatToolbarModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'd3-network-force-zoom-ng';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  nodes!: Array<{ id: string }>;
+  links!: Array<{ source: string, target: string; }>;
+  svg!: any;
+  simulation!: any;
+  node!: any;
+  link!: any;
+  zoom!: any;
+
+  ngOnInit() {
+    this.nodes = [
+      { id: 'A' },
+      { id: 'B' },
+      { id: 'C' },
+      { id: 'D' },
+      { id: 'E' },
+      { id: 'F' }
+    ];
+
+    this.links = [
+      { source: 'A', target: 'B' },
+      { source: 'B', target: 'C' },
+      { source: 'B', target: 'F' },
+      { source: 'D', target: 'E' },
+      { source: 'D', target: 'B' },
+      { source: 'F', target: 'A' },
+    ];
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => this.#initGraph(), 0);
+  }
+
+  #initGraph() {
+    // Create an SVG container for the graph
+    this.svg = select('#network-container')
+      .append('svg')
+      .attr('width', '100%')
+      .attr('height', '100%');
+
+    // Create the links and nodes
+    this.link = this.svg.selectAll('.link')
+      .data(this.links)
+      .enter()
+      .append('line')
+      .attr('class', 'link');
+
+    this.node = this.svg.selectAll('.node')
+      .data(this.nodes)
+      .enter()
+      .append('circle')
+      .attr('class', 'node')
+      .attr('r', 10);
+
+    // Create a simulation for the force-directed layout
+    // this.simulation = forceSimulation(this.nodes as SimulationNodeDatum[])
+    //   .force('link', forceLink(this.links).distance(100))
+    //   .force('charge', forceManyBody().strength(-200))
+    //   .force('center', forceCenter(this.svg.attr('width') / 2, this.svg.attr('height') / 2));
+
+    this.simulation = forceSimulation();
+    this.simulation.nodes(this.nodes);
+    this.simulation
+      .force('link', forceLink())
+      .force('charge', forceManyBody())
+      .force('collide', forceCollide())
+      .force('center', forceCenter())
+      .force('forceX', forceX())
+      .force('forceY', forceY());
+    // apply properties to each of the forces
+    // this._updateForces(this.form.value);
+
+    // Update the simulation on each tick
+    this.simulation.on('tick', () => {
+      if (this.link) {
+        this.link
+          .attr('x1', (d: any) => d.source.x)
+          .attr('y1', (d: any) => d.source.y)
+          .attr('x2', (d: any) => d.target.x)
+          .attr('y2', (d: any) => d.target.y);
+      }
+
+      if (this.node) {
+        this.node
+          .attr('cx', (d: any) => d.x)
+          .attr('cy', (d: any) => d.y);
+      }
+    });
+
+    // Define the zoom behavior
+    this.zoom = zoom()
+      .scaleExtent([ 0.1, 4 ])
+      .on('zoom', (event: any) => {
+        this.svg.attr('transform', event.transform);
+      });
+
+    // Apply the zoom behavior to the SVG container
+    this.svg.call(this.zoom);
+  }
 }
